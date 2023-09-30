28a62d6b89c49258a334a64dcbc6364003e832f0
Thiago Novaes <thiagonovaesb@poli.ufrj.br>
2023-09-30 14:16:34 +0200

    done

--- examples/HydroValleys/case3deterministic.jl
+++ examples/HydroValleys/case3deterministic.jl
@@ -1,137 +1,83 @@
 using GLPK
 using HydroPowerModels
 
-########################################
-#       Load Case
-########################################
+#Simulating the complete horizon
 testcases_dir = joinpath(dirname(dirname(dirname(@__FILE__))), "testcases")
-alldata = HydroPowerModels.parse_folder(joinpath(testcases_dir, "case3deterministic"))
-
-########################################
-#       Set Parameters
-########################################
-# model_constructor_grid may be for example: ACPPowerModel or DCPPowerModel
-# optimizer may be for example: IpoptSolver(tol=1e-6) or GLPK.Optimizer
+alldata = HydroPowerModels.parse_folder(joinpath(testcases_dir, "case3deterministic horizon"))
 params = create_param(;
-    stages=12,
+    stages=60,
     model_constructor_grid=DCPPowerModel,
     post_method=PowerModels.build_opf,
     optimizer=GLPK.Optimizer,
+    discount_factor=0.995,
 );
-
-########################################
-#       Build Model
-########################################
 m = hydro_thermal_operation(alldata, params)
-
-########################################
-#       Solve
-########################################
 HydroPowerModels.train(m; iteration_limit=60);
-
-########################################
-#       Simulation
-########################################
 results = HydroPowerModels.simulate(m, 1);
 
-########################################
-#       Test
-########################################
-# objective
-@test isapprox(
-    sum(s[:stage_objective] for s in results[:simulations][1]), 11000.00, atol=1e-2
-)
-
-# solution
-@test results[:simulations][1][1][:powersystem]["solution"]["bus"]["3"]["deficit"] == 0
-@test isapprox(
-    results[:simulations][1][1][:powersystem]["solution"]["gen"]["2"]["pg"], 0, atol=1e-2
-)
-@test isapprox(
-    results[:simulations][1][1][:powersystem]["solution"]["gen"]["3"]["pg"],
-    0.3756,
-    atol=1e-2,
-)
-@test isapprox(
-    results[:simulations][1][1][:powersystem]["solution"]["gen"]["1"]["pg"],
-    0.6243,
-    atol=1e-2,
-)
-
-########################################
-#       Build Model: min final volume violation
-########################################
+#Simulating from the second year on (tini = 13)
 for data in alldata
-    data["hydro"]["Hydrogenerators"][1]["final_volume"] = 0.4
-    data["hydro"]["Hydrogenerators"][1]["minimal_volume_violation_cost"] = 10000000
+    data["hydro"]["Hydrogenerators"][1]["initial_volume"] = 0.1229422863405989
 end
-
+params = create_param(;
+    stages=48,
+    model_constructor_grid=DCPPowerModel,
+    post_method=PowerModels.build_opf,
+    optimizer=GLPK.Optimizer,
+    discount_factor=0.995,
+);
 m = hydro_thermal_operation(alldata, params)
-
-########################################
-#       Solve
-########################################
 HydroPowerModels.train(m; iteration_limit=60);
-
-########################################
-#       Simulation
-########################################
 results = HydroPowerModels.simulate(m, 1);
 
-########################################
-#       Test
-########################################
-@test results[:simulations][1][end][:reservoirs][:reservoir][1].out >= 0.4
-
-########################################
-#       Build Model: min vol violation
-########################################
+#Simulating shorter horizon with no opporty cost 
+testcases_dir = joinpath(dirname(dirname(dirname(@__FILE__))), "testcases")
+alldata = HydroPowerModels.parse_folder(joinpath(testcases_dir, "case3deterministic horizon"))
 for data in alldata
-    data["hydro"]["Hydrogenerators"][1]["final_volume"] = 0.0
-    data["hydro"]["Hydrogenerators"][1]["min_volume"] = 0.2
-    data["hydro"]["Hydrogenerators"][1]["minimal_volume_violation_cost"] = 10000000
+    data["hydro"]["Hydrogenerators"][1]["final_volume"] = 0.464942
 end
-
+params = create_param(;
+    stages=3,
+    model_constructor_grid=DCPPowerModel,
+    post_method=PowerModels.build_opf,
+    optimizer=GLPK.Optimizer,
+    discount_factor=0.995,
+);
 m = hydro_thermal_operation(alldata, params)
-
-########################################
-#       Solve
-########################################
 HydroPowerModels.train(m; iteration_limit=60);
-
-########################################
-#       Simulation
-########################################
 results = HydroPowerModels.simulate(m, 1);
 
-########################################
-#       Test
-########################################
-@test results[:simulations][1][end][:reservoirs][:reservoir][1].out >= 0.2
+#Simulating the implementation of a policy with K-steps of look-ahead
+testcases_dir = joinpath(dirname(dirname(dirname(@__FILE__))), "testcases")
+alldata = HydroPowerModels.parse_folder(joinpath(testcases_dir, "case3deterministic horizon"))
 
-########################################
-#       Build Model: min turb violation
-########################################
-for data in alldata
-    data["hydro"]["Hydrogenerators"][1]["min_volume"] = 0.0
-    data["hydro"]["Hydrogenerators"][1]["minimal_volume_violation_cost"] = 0.0
-    data["hydro"]["Hydrogenerators"][1]["min_volume"] = 5.0
-    data["hydro"]["Hydrogenerators"][1]["minimal_outflow_violation_cost"] = 10000000
+p = deepcopy(alldata[1]["hydro"]["scenario_probabilities"])
+v = deepcopy(alldata[1]["hydro"]["Hydrogenerators"][1]["inflow"])
+
+t=1
+
+for t = 1:59
+    Tfim = min(59,t+59)
+    Ksteps = Tfim-t+1
+
+    for data in alldata
+        data["hydro"]["scenario_probabilities"] = p[t:end,:]
+        data["hydro"]["Hydrogenerators"][1]["inflow"] = v[t:end,:] * 1.5
+        data["hydro"]["Hydrogenerators"][1]["inflow"][1,1] = data["hydro"]["Hydrogenerators"][1]["inflow"][1,1] / 1.5
+    end
+
+    params = create_param(;
+    stages=Ksteps,
+    model_constructor_grid=DCPPowerModel,
+    post_method=PowerModels.build_opf,
+    optimizer=GLPK.Optimizer,
+    discount_factor=0.995,
+    );
+    m = hydro_thermal_operation(alldata, params)
+    HydroPowerModels.train(m; iteration_limit=60);
+    results = HydroPowerModels.simulate(m, 1);
+
+    for data in alldata
+        data["hydro"]["Hydrogenerators"][1]["final_volume"] = results[:simulations][1][end][:reservoirs][:reservoir][1].out
+    end
 end
-
-m = hydro_thermal_operation(alldata, params)
-
-########################################
-#       Solve
-########################################
-HydroPowerModels.train(m; iteration_limit=60);
-
-########################################
-#       Simulation
-########################################
-results = HydroPowerModels.simulate(m, 1);
-
-########################################
-#       Test
-########################################
-@test results[:simulations][1][end][:reservoirs][:outflow][1] >= 5
